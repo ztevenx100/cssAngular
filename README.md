d85916b9c32df4f151a2d8aa2fe21320a9b7e75e
ztevenx100 <50757337+ztevenx100@users.noreply.github.com>
2019-05-27 15:36:09 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# cssAngular
-Created with CodeSandbox
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
